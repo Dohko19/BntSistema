76cc67726c66ad526f3f0d5fb37298c86eaeac02
Dohko19 <danielkage9@gmail.com>
2020-01-21 16:08:52 -0600

    models

---
+++ app/Http/Controllers/SuaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Sua;
+use Illuminate\Http\Request;
+
+class SuaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Sua  $sua
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Sua $sua)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Sua  $sua
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Sua $sua)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Sua  $sua
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Sua $sua)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Sua  $sua
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Sua $sua)
+    {
+        //
+    }
+}
